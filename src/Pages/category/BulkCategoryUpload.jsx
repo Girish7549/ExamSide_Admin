1223381b68af00eb91baf656f22d1bcef2dfd539
Girish7549 <girish@adzdrio.com>
2025-03-17 15:11:43 +0530

    17-03-25 15:11

--- src/Pages/category/BulkCategoryUpload.jsx
+++ src/Pages/category/BulkCategoryUpload.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
 import {AppSidebar, AppFooter, AppHeader } from '../../components/index';
-import BulkCategoryUploads from '../../components/Category/BulkCategoryUpload';
 
 const BulkCategoryUpload = () => {
   const isAuthenticated = useSelector((state) => state.authenticated);
