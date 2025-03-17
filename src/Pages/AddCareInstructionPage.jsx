14c4752ae0e907da5a8edab2af34221fb53ba911
Girish7549 <girish@adzdrio.com>
2025-03-17 14:11:00 +0530

    2nd commite 17-3-25 14:10

--- src/Pages/AddCareInstructionPage.jsx
+++ src/Pages/AddCareInstructionPage.jsx
@@ -1,21 +1,20 @@
 import React from 'react'
-import {AppContent, AppSidebar, AppFooter, AppHeader } from '../components/index';
-import AddBanner from '../components/Topic/AddBanner';
+import { AppContent, AppSidebar, AppFooter, AppHeader } from '../components/index';
 import AddCareInstruction from '../components/CareInstruction/AddCareInstruction';
 function AddCareInstructionPage() {
   return (
     <>
-         <div>
-      <AppSidebar />
-      <div className="wrapper d-flex flex-column min-vh-100">
-        <AppHeader />
-        <div className="body flex-grow-1">
-        <AddCareInstruction/>
+      <div>
+        <AppSidebar />
+        <div className="wrapper d-flex flex-column min-vh-100">
+          <AppHeader />
+          <div className="body flex-grow-1">
+            <AddCareInstruction />
+          </div>
+          <AppFooter />
         </div>
-        <AppFooter />
-      </div>
 
-    </div>
+      </div>
     </>
   )
 }
