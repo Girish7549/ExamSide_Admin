f08b75331748837bce73a09e6fcc404f64449852
Girish7549 <girish@adzdrio.com>
2025-03-29 16:47:43 +0530

    29-03-25 16:47

--- src/Pages/logo/Logo.jsx
+++ src/Pages/logo/Logo.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { AppSidebar, AppFooter, AppHeader } from '../../components/index';
-import AllLogo from '../../components/Logo/AllLogo';
 function Logo() {
   return (
     <>
@@ -9,7 +8,6 @@ function Logo() {
       <div className="wrapper d-flex flex-column min-vh-100">
         <AppHeader />
         <div className="body flex-grow-1">
-        <AllLogo/>
         </div>
         <AppFooter />
       </div>
